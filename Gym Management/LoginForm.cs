64847f2d5541b44072b5c0a712e209b024fcbe63
as-1771 <footballox10@gmail.com>
2025-12-30 05:43:15 -0800

    Edited File Name Form1 to LoginForm

---
+++ Gym Management/LoginForm.cs
@@ -0,0 +1,35 @@
+namespace Gym_Management
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
