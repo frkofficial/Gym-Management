4bfae4343dfdc0857b72ed684e78c0185bf8c6eb
as-1771 <footballox10@gmail.com>
2026-01-24 21:46:09 -0800

    commiting Payment control

---
+++ Gym Management/Entities/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management.Entities
+{
+    public class Payment
+    {
+
+        public int PaymentID { get; set; }
+        public int CustomerID { get; set; }
+        public int PackageId { get; set; }
+        public string CustomerName { get; set; }
+        public decimal PackageAmount { get; set; }
+        public decimal TrainerPriceAmount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime PayingDate { get; set; }
+        public string Status { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
